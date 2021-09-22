5d8a3a2dee9bb7f79401dfe8b84cf50c5fda3e29
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-09-22 15:41:59 -0500

    wip

---
+++ data/db-config.js
@@ -0,0 +1,6 @@
+const knex = require('knex');
+
+const configurations = require('../knexfile.js');
+const enviroment = process.env.NODE_ENV
+
+module.exports = knex(configurations[enviroment]);
---
+++ knexfile.js
@@ -0,0 +1,21 @@
+const sharedConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  //sqlite3 stuff below here
+  migrations: { direcotry: "./data/migrations" },
+  pool: {
+    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+  },
+};
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: { filename: "./data/cook_book.db3" },
+    seeds: { directory: "./data/seeds" },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: "./data/cook_book.test.db3" },
+  },
+};
