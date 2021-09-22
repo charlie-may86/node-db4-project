7cb8d51712e4757420e8eab31881d6c35637b5a8
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-09-22 14:43:47 -0500

    wip

--- api/recipies/recipies-router.js
+++ api/recipies/recipies-router.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
+const Recipe = require('./recipies-model')
 
-router.use("*", (req, res, ) => {
-  res.json({ api: "up" });
-});
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(next)
+})
+
 
 router.use((err, req, res, next) => {  //eslint-disable-line
-
   res.status(500).json({
     customMessage: "something went wrong inside the recipes router",
     message: err.message,
